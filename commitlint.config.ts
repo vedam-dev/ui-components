6f051bba9eacff19480d41afdb04afa2eb10553b
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-06-18 15:11:43 +0530

    feat(VF-36): add husky

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,23 +1,3 @@
-// import type { UserConfig } from '@commitlint/types';
-
-// const config: UserConfig = {
-//   extends: ['@commitlint/config-conventional'],
-//   parserPreset: {
-//     parserOpts: {
-//       headerPattern: /^([a-z]+)\(([A-Z]+-\d+)\):\s(.+)$/,
-//       headerCorrespondence: ['type', 'scope', 'subject']
-//     }
-//   },
-//   rules: {
-//     'scope-empty': [2, 'never']
-//   },
-//   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-//   ignores: [message => message.includes('[jenkins-commit]')]
-// };
-
-// export default config;
-
-
 import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
@@ -31,8 +11,8 @@ const config: UserConfig = {
   rules: {
     'scope-empty': [2, 'never']
   },
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   ignores: [message => message.includes('[jenkins-commit]')]
 };
 
 export default config;
-
