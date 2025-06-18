7613ee5df813b71369b3b9889306c3033e4f6d1c
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-06-18 12:37:30 +0530

    feat(VF-36): migrate husky hooks to v10 format

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,23 @@
+// import type { UserConfig } from '@commitlint/types';
+
+// const config: UserConfig = {
+//   extends: ['@commitlint/config-conventional'],
+//   parserPreset: {
+//     parserOpts: {
+//       headerPattern: /^([a-z]+)\(([A-Z]+-\d+)\):\s(.+)$/,
+//       headerCorrespondence: ['type', 'scope', 'subject']
+//     }
+//   },
+//   rules: {
+//     'scope-empty': [2, 'never']
+//   },
+//   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+//   ignores: [message => message.includes('[jenkins-commit]')]
+// };
+
+// export default config;
+
+
 import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
@@ -11,8 +31,8 @@ const config: UserConfig = {
   rules: {
     'scope-empty': [2, 'never']
   },
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   ignores: [message => message.includes('[jenkins-commit]')]
 };
 
 export default config;
+
