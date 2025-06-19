cdc9135480cbcf8e3bee6632089cbd5ce605b325
Naman Punn <naman.punn@vedam.org>
2025-06-19 14:09:53 +0530

    VF-25-finalcheck

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-export { default as Avatar } from './component/atom/avatar/Avatar';
-export {default as Typography } from './component/atom/typography';
+export { default as Avatar } from "./component/atom/avatar/Avatar"
+export { default as Typography } from './component/atom/typography/Typography';
+export { default as Box } from './component/atom/box/Box';
+export { default as Button } from './component/atom/button/Button';
+export { default as Paper } from './component/atom/paper/Paper';
+export { default as Banner } from './component/molecule/banner/Banner';
+export { default as HorizontalBaseCard } from './component/organism/card/HorizontalBaseCard';
+export { default as VerticalBasecard} from './component/organism/card/VerticalBasecard';
+export { default as defaultTheme } from './theme/customer/default';
+export { default as wvTheme } from './theme/customer/wv';
+export { default as ksTheme } from './theme/customer/ks';
+export { default as ks3Theme } from './theme/customer/ks3';
