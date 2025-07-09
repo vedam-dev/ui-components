622cee68e23882927125cf244d54e307d5bc8f60
HRBvedam <hrishabh.bharati@vedam.org>
2025-07-09 12:05:45 +0530

    VF-61 => resolving conflict

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,10 +4,50 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/material-icons';
 
+import '@fontsource/lato/100-italic.css';
+import '@fontsource/lato/300-italic.css';
+import '@fontsource/lato/400-italic.css';
+import '@fontsource/lato/700-italic.css';
+import '@fontsource/lato/900-italic.css';
+
+import '@fontsource/lato/100.css';
 import '@fontsource/lato/300.css';
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/700.css';
+import '@fontsource/lato/900.css';
+import '@fontsource/montserrat/800.css';
+import '@fontsource/montserrat/900.css';
 
+import '@fontsource/montserrat/100-italic.css';
+import '@fontsource/montserrat/200-italic.css';
+import '@fontsource/montserrat/300-italic.css';
+import '@fontsource/montserrat/400-italic.css';
+import '@fontsource/montserrat/500-italic.css';
+import '@fontsource/montserrat/600-italic.css';
+import '@fontsource/montserrat/700-italic.css';
+import '@fontsource/montserrat/800-italic.css';
+import '@fontsource/montserrat/900-italic.css';
+
+import '@fontsource/outfit/100.css';
+import '@fontsource/outfit/200.css';
+import '@fontsource/outfit/300.css';
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/500.css';
+import '@fontsource/outfit/600.css';
+import '@fontsource/outfit/700.css';
+import '@fontsource/outfit/800.css';
+import '@fontsource/outfit/900.css';
+
+import '@fontsource/nunito-sans/200.css';
+import '@fontsource/nunito-sans/300.css';
+import '@fontsource/nunito-sans/400.css';
+import '@fontsource/nunito-sans/600.css';
+import '@fontsource/nunito-sans/700.css';
+import '@fontsource/nunito-sans/800.css';
+import '@fontsource/nunito-sans/900.css';
+
+import '@fontsource/montserrat/100.css';
+import '@fontsource/montserrat/200.css';
 import '@fontsource/montserrat/300.css';
 import '@fontsource/montserrat/400.css';
 import '@fontsource/montserrat/500.css';
