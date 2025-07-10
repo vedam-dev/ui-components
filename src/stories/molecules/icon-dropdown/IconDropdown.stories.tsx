d9aa7cf5a1e50927cb060bb825a76b72621af721
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2025-07-10 15:43:46 +0530

    Feature/vf 105 icon dropdown (#36)
    * VF-105 icon-dropdown layout created
    
    * VF-105 icon dropdown implemented

---
+++ src/stories/molecules/icon-dropdown/IconDropdown.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react';
+import IconDropdown from '../../../component/molecule/icon-dropdown/IconDropdown';
+
+const meta: Meta<typeof IconDropdown> = {
+  title: 'Molecule/IconDropdown',
+  component: IconDropdown,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof IconDropdown>;
+
+export const MonthDropdown: Story = {
+  render: (args) => <IconDropdown {...args} />,
+  args: {
+    label: 'June 2025',
+    iconUrl:
+      'https://acjlsquedaotbhbxmtee.supabase.co/storage/v1/object/public/vedam-website-assets/images/certificate/Vector-2.jpg',
+    options: ['May 2025', 'June 2025', 'July 2025'],
+  },
+};
+
+export const SemesterDropdown: Story = {
+  render: (args) => <IconDropdown {...args} />,
+  args: {
+    label: 'Semester',
+    iconUrl:
+      'https://acjlsquedaotbhbxmtee.supabase.co/storage/v1/object/public/vedam-website-assets/images/certificate/Vector-3.jpg',
+    options: ['Semester 1', 'Semester 2', 'Semester 3'],
+  },
+};
