c7d1b1dc8b898ea81fcc3e691f960d368b4bd413
Naman Punn <naman.punn@vedam.org>
2025-07-14 16:56:27 +0530

    VF-102 undefined colour issue resolved

--- src/theme/core-theme.tsx
+++ src/theme/core-theme.tsx
@@ -46,28 +46,28 @@ export const createCoreTheme = (options: CoreThemeOptions): CoreTheme => {
 
 declare module '@mui/material/styles' {
   interface PaletteColor {
-    50?: string;
-    100?: string;
-    200?: string;
-    300?: string;
-    400?: string;
-    500?: string;
-    600?: string;
-    700?: string;
-    800?: string;
-    900?: string;
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
   }
 
   interface SimplePaletteColorOptions {
-    50?: string;
-    100?: string;
-    200?: string;
-    300?: string;
-    400?: string;
-    500?: string;
-    600?: string;
-    700?: string;
-    800?: string;
-    900?: string;
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
   }
 }
