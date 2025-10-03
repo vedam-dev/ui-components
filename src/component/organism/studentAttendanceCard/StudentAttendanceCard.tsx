af4ac2a69155d6c2506f9438c13d16f4be8028e5
naman-vedam <naman.punn@vedam.org>
2025-10-03 19:31:17 +0530

    VF-285 buf fix (#88)

---
+++ src/component/organism/studentAttendanceCard/StudentAttendanceCard.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { CoreTheme, useCoreTheme } from '../../../theme/core-theme';
+
+interface StudentAttendanceCardProps {
+  name: string;
+  email: string;
+  attendance: number;
+}
+
+const StudentAttendanceCard: React.FC<StudentAttendanceCardProps> = ({
+  name,
+  email,
+  attendance,
+}) => {
+  const theme = useCoreTheme() as CoreTheme;
+  return (
+    <Box
+      sx={{
+        bgcolor: 'white',
+        borderRadius: theme.spacing(6),
+        boxShadow: '0 2px 8px rgba(0,0,0,0.06)',
+        py: theme.spacing(5.5),
+        pl: theme.spacing(12.5),
+        pr: theme.spacing(34),
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        transition: 'box-shadow 0.2s',
+      }}
+    >
+      <Box>
+        <Typography
+          sx={{
+            color: '#6513AC',
+            fontFamily: 'Outfit',
+            fontSize: '18px',
+            fontStyle: 'normal',
+            fontWeight: 600,
+            lineHeight: 'normal',
+            mb: '8px',
+          }}
+        >
+          {name}
+        </Typography>
+        <Typography
+          sx={{
+            color: '#000',
+            fontFamily: 'Outfit',
+            fontSize: '16px',
+            fontStyle: 'normal',
+            fontWeight: 500,
+            lineHeight: 'normal',
+          }}
+        >
+          {email}
+        </Typography>
+      </Box>
+
+      <Box sx={{ textAlign: 'right' }}>
+        <Typography
+          sx={{
+            color: '#777',
+            fontFamily: 'Outfit',
+            fontSize: '16px',
+            fontStyle: 'normal',
+            fontWeight: 500,
+            lineHeight: 'normal',
+            mb: '8px',
+          }}
+        >
+          Average Attendance
+        </Typography>
+        <Typography
+          sx={{
+            color: '#8A18FF',
+            fontFamily: 'Outfit',
+            fontSize: '18px',
+            fontStyle: 'normal',
+            fontWeight: 600,
+            textAlign: 'left',
+            lineHeight: 'normal',
+          }}
+        >
+          {attendance}%
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default StudentAttendanceCard;
