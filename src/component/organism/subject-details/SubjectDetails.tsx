2e46cb4e98c2f2b9d34e1f7a98c60c12eea11847
ankitranjan1028-vedam <ankit.ranjan@vedam.org>
2026-04-06 11:20:49 +0530

    VLMS-482 arrow button variant created

--- src/component/organism/subject-details/SubjectDetails.tsx
+++ src/component/organism/subject-details/SubjectDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { CoreTheme, useCoreTheme } from '../../../theme/core-theme';
+import { ArrowButton } from '../../atom/button';
 
 export interface ClassListItemRowProps {
   title: string;
@@ -146,48 +147,7 @@ export interface ClassListItemArrowProps {
 }
 
 export const ClassListItemArrow: React.FC<ClassListItemArrowProps> = ({ onClick }) => {
-  const theme = useCoreTheme() as CoreTheme;
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        cursor: onClick ? 'pointer' : 'default',
-        flex: '0 0 auto',
-      }}
-      onClick={onClick}
-    >
-      <Box
-        sx={{
-          width: '39px',
-          height: '39px',
-          borderRadius: '39px',
-          background: theme.palette.primary[100],
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          '&:hover': onClick
-            ? {
-                background: theme.palette.primary[200],
-              }
-            : {},
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="10"
-          height="19"
-          viewBox="0 0 10 19"
-          fill="none"
-        >
-          <path
-            d="M9.75562 10.0788L1.42322 18.2602C1.3458 18.3363 1.2539 18.3965 1.15275 18.4377C1.0516 18.4788 0.943187 18.5 0.833703 18.5C0.72422 18.5 0.615808 18.4788 0.514659 18.4377C0.413509 18.3965 0.321602 18.3363 0.244186 18.2602C0.166769 18.1842 0.105359 18.094 0.0634615 17.9947C0.021564 17.8953 0 17.7889 0 17.6814C0 17.5739 0.021564 17.4675 0.0634615 17.3681C0.105359 17.2688 0.166769 17.1786 0.244186 17.1026L7.98811 9.5L0.244186 1.89743C0.0878359 1.74391 -1.64741e-09 1.5357 0 1.3186C1.64741e-09 1.10149 0.0878359 0.893277 0.244186 0.739761C0.400535 0.586245 0.612592 0.5 0.833703 0.5C1.05482 0.5 1.26687 0.586245 1.42322 0.739761L9.75562 8.92116C9.83309 8.99715 9.89455 9.08738 9.93649 9.1867C9.97842 9.28602 10 9.39248 10 9.5C10 9.60752 9.97842 9.71398 9.93649 9.8133C9.89455 9.91262 9.83309 10.0028 9.75562 10.0788Z"
-            fill={theme.palette.primary[500]}
-          />
-        </svg>
-      </Box>
-    </Box>
-  );
+  return <ArrowButton onClick={onClick} />;
 };
 
 // Divider component for the vertical line
